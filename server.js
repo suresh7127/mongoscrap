ba5c53f1938915f8110bb23297dfc2ef337930a7
suresh <sureshmahato@Sureshs-MacBook-Pro.local>
2019-10-04 07:38:38 -0500

    models and server code modified with new repo

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+
+// Our scraping tools
+var request = require("request");
+var cheerio = require("cheerio");
+
+// Require all models
+var db = require("./models");
+
+var PORT = process.env.PORT || 3000;
+
+// Initialize Express
+var app = express();
+
+// Configure middleware
+
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Use body-parser for handling form submissions
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+// Use express.static to serve the public folder as a static directory
+app.use(express.static("public"));
